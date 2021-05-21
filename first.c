6c007bf6ab603dfb65bd6cab6ac33c44c2ae0e68
NARESH <nareshshet143@gmail.com>
2021-05-21 18:06:43 +0530

    ADDED SOME NEW FILES

---
+++ first.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+ int main ()
+ {
+     printf("HEY WE ARE HERE TO HELP YOU");
+     return 0;
+ }
---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"NARESH SHET" <<endl;
+    return 0;
+}
